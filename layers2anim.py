6badfce6a0e0a458078ebd333f570e0e9e0d19d3
anonymou8 <64681993+anonymou8@users.noreply.github.com>
2020-10-27 13:27:41 +0500

    sublayers handling

--- layers2anim.py
+++ layers2anim.py
@@ -21,23 +21,52 @@ class AnimLayers(TempDirMixin, inkex.OutputExtension):
             inkex.errormsg("No layers found.")
             return
 
-        for node in layers:
-            node.style['display'] = "none"
-        
-        for i,node in enumerate(layers):
-            node.style.update("display:inherit;opacity:1;")
+        def show_layer(layer, show=True):
+            if show:
+                layer.style.update('display:inherit;opacity:1;')
+            else:
+                layer.style.update('display:none;')
 
-            filename = take_snapshot(self.document, dirname=self.tempdir,
-                                     name=f'{i:04}', ext='png',
-                                     dpi=self.options.resolution)
-            node.style['display'] = "none"
+        def show_parent_layers(layer, show=True):
+            parent = layer
+            while True:
+                parent = parent.getparent()
+                if parent.get('inkscape:groupmode') == 'layer':
+                    show_layer(parent, show=show)
+                else:
+                    break
+        
+        # first, hide everything
+        for layer in layers:
+            show_layer(layer, show=False)
+        
+        # if first layer is named 'bg' then show it forever
+        if layers[0].get('inkscape:label') == 'bg':
+            show_layer(layers[0])
+            layers = layers[1:]
+        
+        i = 0
+        for layer in layers:
+            # do not generate an image for layers that have sublayers
+            children = layer.getchildren()
+            if children and children[0].get('inkscape:groupmode') == 'layer':
+                continue
             
+            show_parent_layers(layer)
+            show_layer(layer)
+            filename = take_snapshot(self.document, dirname=self.tempdir, name=f'{i:04}', ext='png', dpi=self.options.resolution)
+            show_layer(layer, show=False)
+            show_parent_layers(layer, show=False)
+            
+            # copy PNG file if the option enabled
             if self.options.pngs and self.options.path:
                 f = os.path.join(self.tempdir, f'{i:04}.png')
                 t = os.path.join(self.options.path, f'{i+self.options.pngn:04}.png')
                 shutil.copyfile(f, t)
+                
+            i += 1
         
-        # Convert to mp4
+        # convert to mp4
         fps = self.options.fps
         crf = self.options.crf
         codec = ['libx264', 'libx264rgb'][self.options.rgb]
@@ -48,6 +77,7 @@ class AnimLayers(TempDirMixin, inkex.OutputExtension):
         args = f'ffmpeg {info} -r {fps} -i {pngs} {loops} -c:v {codec} -crf {crf} {mp4}'
         sp.run(args.split())
         
+        # save output
         with open(mp4, 'rb') as tmp4:
             shutil.copyfileobj(tmp4, stream)
 
