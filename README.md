2f4cb26a076654e48580a3babd7f31b2db03965b
anonymou8 <64681993+anonymou8@users.noreply.github.com>
2023-08-31 19:23:02 +0500

    Version 2

--- README.md
+++ README.md
@@ -1,61 +1,121 @@
-## Inkscape extension to convert SVG document to MP4-animation
+## Inkscape extension to convert SVG document layers to animation (*version 2*)
 
-Works with Inkscape v.1.2*
+Tested on Inkscape 1.2 and 1.3.
 
-\* — *(There's an [issue](https://gitlab.com/inkscape/inkscape/-/issues/2473) on some OSes
-with missing `cssselect` Python module, on which Inkscape extensions subsystem depends. 
-Manual installation fixes the problem. It may be named `python3-cssselect` 
-or just `python-cssselect` in your distro repository.)*
+### New in version 2
+
+- Extension moved to the `Extensions` menu under `Export` submenu
+- New dialog with video format selection
+- New video formats: MP4 (H.264), WebM (VP9), ProRes, animated PNG (APNG), PNG, GIF
+- Transparency can be enabled for formats supporting it (WebM, ProRes, (A)PNG, GIF)
+- FPS and scale (former "resolution") input number types changed to floats
+- Fixed linked images not being rendered bug
+
 
 ### Description
 
+This extension creates frame-by-frame animation from an SVG document layers. 
+Output formats are MP4, WebM, ProRes, GIF, APNG or just PNG image sequence.
+
+To use the extension, install it (see instruction below), restart Inkscape and navigate
+to `Extensions → Export → Export layers as animation frames...`
+
 <img src="https://raw.githubusercontent.com/anonymou8/inkscape-animation/main/screenshot.png" />
+<!-- ![screenshot](screenshot.png) -->
 
 Each layer in a document represents a single frame. Bottom layer is the first frame.
+Layers can be nested to create scenes.
 
-If a layer name is one of `bg`, `fg`, `show`, `nohide`, `backgroud` or `foreground` then it 
-will be visible throughout animation, and if name is one of `hide`, `hidden`, `scratch`, `temp` 
-or`guides` then it will always be hidden.
+If a layer name is one of `bg`, `fg`, `background` or `foreground` then it 
+will be visible throughout animation, and if the name is one of 
+`hide`, `hidden`, `off` or `guides` then it will always be hidden. Another way
+to change layer visibility is to add any of the keywords in square brackets to layer's
+name, for example: `Layer 1 [bg]`, `Scene 2 [off]`.
 
-The extension is of Export type. To use it go to `File → Save As...` or `File → Save a Copy...`
-(preferred) and select `Layers as animation frames (ffmpeg) (*.mp4)` for the format.
+Layers may be nested to organize, for example, separate scenes. Parent layers act like foreground. 
+Only leaf layers will be treated as frames.
 
-The extension uses `ffmpeg` to combine PNG images of layers to MP4 file.
-User is able to select rendering resolution, framerate and encoding quality (CRF). 
-`Loops` parameter tells ffmpeg how many times to repeat a frame sequence. The codec used is h264.
-
-`Set layer opacity to 100%` is useful when you use layer's opacity for onioin skinning, 
-then the option will set opacity of each layer to 100% during export.
+#### Parameters description
 
 `Treat first level objects as frames` option tells the extension to make frames from
-objects themselves and not layers. Only direct children of a layer can be frames. This
-is useful for [quick animations](https://raw.githubusercontent.com/anonymou8/inkscape-animation/main/quick.gif) 
-on a single layer. E.g. you can interpolate subpaths with LPE, convert it regular path, 
-then break it apart and use the extension without dealing with layers.
+objects themselves and not from layers. This may be useful for quick animations. Only direct 
+children of a layer can be frames. The first frame is an object with the lowest z-order.
+Clones only supported if their original is labeled with one of the "always exposed" names (see above).
+Object label can also be used to set background or hide the object in animation.
 
-<!-- <img src="https://raw.githubusercontent.com/anonymou8/inkscape-animation/main/quick.gif" /> -->
+`Frame scale` parameter sets video dimensions in percents of a document size. 
+For document size in absolute units: one inch equals to 96 pixels and 1 mm equals to 
+`96/25.4 = ~3.78` pixels.
 
-There is an option that allows to save each frame in PNG format to a specified directory. Files will
-be named in four-digit format, like `0000.png`, `0001.png` e.t.c. User can change first image number.
-Directory will be created if doesn't exist.
+`Framerate` — animation speed in frames per second. FPS can be less than 1, this may be useful
+for a simple slideshow.
+
+`Loops` — how many times to repeat frames sequence. APNG and GIF formats ignore the parameter
+and always loop infinitely.
+
+`Back and forth` — append reversed frame sequence for a "ping-pong" animation. Only applicable
+if frames count more than 2. Final frame count will be original frame count x2 minus 2.
+
+`Set layer opacity to 100%` option is useful when you use a layer opacity for onion skinning, 
+if the option is enabled then it will set opacity of each layer to 100% during export.
+
+`Page transparency` option allows to create animations with transparency if supported by a format.
+
+With `Use 'rsvg-convert' as renderer` option `rsvg-convert` tool from [Librsvg](https://github.com/GNOME/librsvg)
+is used as SVG renderer. While it is faster than Inkscape it supports different subset
+of the SVG specification and the render may look different for some documents.
+For this option to work, Librsvg must be installed separately. On some systems `librsvg` 
+package is required for GTK, GIMP or Ffmpeg, so there is a chance it is already installed 
+on your system.
+
+`Codec` — a video format your animation will be exported to.
+
+`MP4 / WebM quality, CRF` parameter sets picture quality for MP4 and WebM formats.
+Lower CRF means better quality. MP4 range is 0-51, WebM range is 0-63, good quality values are 23 and 32
+respectively. For MP4, CRF=0 produces lossless output.
+
+`Output filename` — a path to an existing or new file or folder (in case of PNG sequence)
+where the animation will be saved. Extension may be omitted.
+
+If `PNG sequence` is chosen as a codec then each layer will be saved
+as a separate PNG with a four-digit name like `0000.png`, `0001.png`, `0002.png` e.t.c, 
+The `PNG sequence first image number` parameter sets the initial file number.
+
+If `Play looped animation after export` option is enabled then after saving an animation 
+it will be played with the `ffplay` tool from Ffmpeg. Some useful `ffplay` hotkeys:
+
+    f           - Toggle fullscreen
+    Space       - Pause
+    s           - Step single frame (forward only)
+    Right, Left - Seek ±10 seconds
+    RMB         - Seek to percentage in file corresponding to mouse position
+    q, Esc      - Quit
 
 ### Installation
 
-You must have [ffmpeg](https://ffmpeg.org/) executable installed. Make sure a path to it is 
-mentioned in the system global `PATH` variable. As newer version of Inkscape is bundled with 
-Python 3, you don't have to worry about the interpreter.
-
-Copy `layers2anim.inx` and `layers2anim.py` to extensions directory (or to its subdirectory) 
-and restart Inkscape. You can find out the path to extensions directory in 
+Copy `layers2anim.inx` and `layers2anim.py` to the extensions directory or its subdirectory
+and restart Inkscape. To find out the path to the extensions directory navigate to
 `Edit → Preferences → System: User extensions`.
 
-### Example animation 
+You must have [ffmpeg](https://ffmpeg.org/) executable installed. Make sure a path to it is 
+mentioned in the system global `PATH` variable. On a UNIX-like system you may check which tools 
+are installed by entering next command in a terminal:
+
+```
+which ffmpeg ffplay rsvg-convert
+```
+
+An output might look like this:
+
+```
+/usr/bin/ffmpeg
+/usr/bin/ffplay
+/usr/bin/rsvg-convert
+```
+
+The output tells that required (`ffmpeg`) and optional (`ffplay` and `rsvg-convert`) executables 
+are installed on the system.
+
+### Example animation
 
 <img src="https://raw.githubusercontent.com/anonymou8/inkscape-animation/main/swan.gif" />
-
-This GIF was made out of PNGs exported by the extension with these commands:
-
-```
-ffmpeg -i %04d.png -vf palettegen=max_colors=256 palette.png
-ffmpeg -r 18 -i %04d.png -i palette.png -lavfi paletteuse=bayer:4 swan.gif
-```
--- layers2anim.py
+++ layers2anim.py
@@ -1,141 +1,315 @@
+import sys, os, subprocess, tempfile
+from itertools import filterfalse
+from shutil import copyfile
 import inkex
-from inkex.base import TempDirMixin
-from inkex.command import take_snapshot as inkscape_snapshot, write_svg
-import os, subprocess as sp, shutil, sys
+from inkex.base import (
+    InkscapeExtension, TempDirMixin,
+    SvgInputMixin, SvgOutputMixin
+)
+from inkex.command import inkscape
 
-class AnimLayers(TempDirMixin, inkex.OutputExtension):
+Mixins = (SvgInputMixin, SvgOutputMixin, TempDirMixin)
+class Layers2Anim(*Mixins, InkscapeExtension):
     def add_arguments(self, pars):
-        pars.add_argument('--resolution', type=int, default=96)
-        pars.add_argument('--fps', type=int, default=12)
-        pars.add_argument('--loops', type=int, default=1)
-        pars.add_argument('--crf',  type=int, default=22)
-        pars.add_argument('--opac', type=inkex.Boolean, default=True)
         pars.add_argument('--firstlev', type=inkex.Boolean, default=False)
-        pars.add_argument('--rgb',  type=inkex.Boolean, default=True)
+        pars.add_argument('--scale', type=float, default=100)
+        pars.add_argument('--fps', type=float, default=12)
+        pars.add_argument('--loops', type=int, default=1)
+        pars.add_argument('--backandforth', type=inkex.Boolean, default=False)
+        
+        pars.add_argument('--opac', type=inkex.Boolean, default=True)
+        pars.add_argument('--tran', type=inkex.Boolean, default=False)
         pars.add_argument('--rsvg', type=inkex.Boolean, default=True)
-        pars.add_argument('--pngs', type=inkex.Boolean, default=False)
+        
+        pars.add_argument('--codec', type=str, default='mp4')
+        pars.add_argument('--crf',  type=int, default=23)
+        
         pars.add_argument('--path', type=str, default='')
         pars.add_argument('--pngn', type=int, default=0)
         pars.add_argument('--ffplay', type=inkex.Boolean, default=False)
+        
+        pars.add_argument('--tab') # not used
 
-    def save(self, stream):
-        layers = self.svg.xpath("//svg:g[@inkscape:groupmode='layer']")
+    def effect(self):
+        opts = self.options
+        
+        def get_layers(element):
+            return element.xpath("svg:g[@inkscape:groupmode='layer']")
 
-        if len(layers) < 1:
+        def is_layer(element):
+            return element.get('inkscape:groupmode') == 'layer'
+
+        if not get_layers(self.svg):
             inkex.errormsg("No layers found.")
-            return
+            return False
 
-        if self.options.pngs:
-            if not self.options.path:
-                inkex.errormsg("PNG frames will not be saved. Please, choose a path.")
-                # TODO: export MP4 anyway
-                return
-            if not os.path.exists(self.options.path):
-                os.mkdir(self.options.path)
-            elif not os.path.isdir(self.options.path):
-                inkex.errormsg(f'"{self.options.path}" is not a directory.')
-                return
+        def can_write(path):
+            while True:
+                if os.path.exists(path) or not path:
+                    break
+                path = os.path.split(path)[0]
+            return path and os.access(path, os.W_OK)
 
-        exposed_layer_names = 'bg', 'background', 'fg', 'foreground', 'show', 'nohide'
-        hidden_layer_names = 'hide', 'hidden', 'scratch', 'temp', 'guides'
-
-        def label(layer):
-            layer_label = layer.get('inkscape:label')
-            return layer_label and layer_label.lower() or ''
-
-        def show_layer(layer, show=True):
-            if show or label(layer) in exposed_layer_names:
-                layer.style.update('display:inherit;' + 'opacity:1;' * self.options.opac)
+        def check_path():
+            if not opts.path:
+                inkex.errormsg("Please, choose an output filename/folder.")
+                return False
+            if opts.path.startswith('~') and os.getenv('HOME'):
+                opts.path = opts.path.replace('~', os.getenv('HOME'), 1)
+            opts.path = os.path.realpath(opts.path)
+            if opts.codec == 'png':
+                if os.path.exists(opts.path) and not os.path.isdir(opts.path):
+                    inkex.errormsg(f'"{opts.path}" exists an it is not a folder.'
+                                    ' Please, choose a folder for PNG sequence.')
+                    return False
             else:
-                layer.style.update('display:none;')
+                if os.path.exists(opts.path) and os.path.isdir(opts.path):
+                    inkex.errormsg(f'"{opts.path}" is an existing folder.'
+                                    ' Please, choose a file name to save animation to.')
+                    return False
+            if not can_write(opts.path):
+                inkex.errormsg(f'Could not write to "{opts.path}".'
+                                ' Please choose another location.')
+                return False
+            
+        if check_path() is False:
+            return False
 
-        def is_layer(obj):
-            return obj.get('inkscape:groupmode') == 'layer'
+    # layer manipulation functions
+        # hidden layer tags have priority over exposed
+        hidden_layer_tags = 'hide', 'hidden', 'off', 'temp', 'guides'
+        exposed_layer_tags = 'bg', 'background', 'fg', 'foreground'
+
+        def layer_label_in(layer, tags):
+            label = layer.get('inkscape:label')
+            label = (label or layer.get('id') or '').lower()
+            return label in tags or any(f'[{tag}]' in label for tag in tags)
+
+        def show_element(element, show=True):
+            if layer_label_in(element, exposed_layer_tags):
+                element.style.update('display:inherit;')
+            elif show and not layer_label_in(element, hidden_layer_tags):
+                element.style.update('display:inherit;' + 'opacity:1;'*opts.opac)
+            else:
+                element.style.update('display:none;')
 
         def show_parent_layers(layer, show=True):
             parent = layer
             while True:
                 parent = parent.getparent()
-                if is_layer(parent):
-                    show_layer(parent, show=show)
-                else:
+                if not is_layer(parent):
                     break
+                show_element(parent, show=show)
 
-        def rsvg_snapshot(document, dirname, name, dpi, **kwargs):
-            path = os.path.join(dirname, name)
-            width = round(self.svg.viewport_width * dpi / 96)
-            bg = self.svg.namedview.get('pagecolor')
-            write_svg(document, path+'.svg')
-            args = f'rsvg-convert -w {width} -a -b {bg} -o {path}.png {path}.svg'
-            sp.run(args.split())
+    # render functions
+        svg_path = os.path.split(opts.input_file)[0]
+        formats_with_alpha = ['webm_420', 'prores', 'png', 'apng', 'gif']
+        opts.tran = opts.tran and opts.codec in formats_with_alpha
+        
+        def execute(args, stdout=sys.stderr):
+            if not stdout:
+                stdout = subprocess.DEVNULL
+            subprocess.run(args, stdout=stdout)
+        
+        # for hrefs to work a frame must be saved under the original path
+        def write_temp_svg(document):
+            'Returns a temporary filename to remove later'
+            fd, name = tempfile.mkstemp(dir=svg_path, prefix='ink_ext_l2a_', 
+                                        suffix='.svg')
+            f = os.fdopen(fd, 'wb')
+            document.write(f)
+            f.close()
+            return name
 
-        if self.options.rsvg:
-            snapshot = rsvg_snapshot
+        def rsvg_snapshot(svg_name, png_name):
+            width = round(self.svg.viewport_width * opts.scale / 100)
+            bg = 'none' if opts.tran else self.svg.namedview.get('pagecolor')
+            execute(['rsvg-convert', '-a', '-w', str(width), '-b', bg, 
+                     '-o', png_name, svg_name])
+            
+        def inkscape_snapshot(svg_name, png_name):
+            dpi = 96 * opts.scale / 100
+            opacity = 0 if opts.tran else 255
+            inkscape(svg_name, export_filename=png_name, 
+                     export_dpi=dpi, export_background_opacity=opacity)
+
+        if opts.rsvg:
+            try:
+                execute(['rsvg-convert', '-v'], None)
+                snapshot = rsvg_snapshot
+            except FileNotFoundError:
+                inkex.errormsg("`rsvg-convert` not found,"
+                               " Inkscape is used as renderer.")
+                snapshot = inkscape_snapshot
         else:
             snapshot = inkscape_snapshot
 
-        # filter frame and bg layers
+        def export_png(i, total_count):
+            svg_name = write_temp_svg(self.document)
+            png_name = os.path.join(self.tempdir, f'{i:04}.png')
+            snapshot(svg_name, png_name)
+            if opts.backandforth and total_count > 2:
+                if i in range(1, total_count-1):
+                    i = total_count * 2 - 2 - i
+                    png2_name = os.path.join(self.tempdir, f'{i:04}.png')
+                    copyfile(png_name, png2_name)
+            os.remove(svg_name)
+        
+    # filter frame layers or objects
         frame_layers = []
 
-        def filter_layers(layers, firstlev=False):
-            for layer in layers:
-                # hide everything except bg
-                show_layer(layer, show=False)
-                # select regular leaf layers or their first level objects
-                if label(layer) not in [*hidden_layer_names, *exposed_layer_names]:
-                    children = layer.getchildren()
-                    has_children_layers = any(is_layer(c) for c in children)
-                    if has_children_layers:
+        def filter_objects(elements, show=False):
+            'Frame is any frirst level object'
+            for element in elements:
+                if is_layer(element):
+                    show_element(element, show=True)
+                    if layer_label_in(element, hidden_layer_tags):
                         continue
-                    elif firstlev:
-                        filter_layers(children)
-                    else:
-                        frame_layers.append(layer)
+                    show_next = layer_label_in(element, exposed_layer_tags)
+                    filter_objects(element.getchildren(), show_next)
+                else:
+                    show_element(element, show)
+                    if show == False:
+                        special_tags = hidden_layer_tags + exposed_layer_tags
+                        if not layer_label_in(element, special_tags):
+                            frame_layers.append(element)
+            
+        def filter_layers(layers, show=False, append=True):
+            'Frame is only a leaf layer'
+            for layer in layers:
+                show_element(layer, show)
+                if layer_label_in(layer, hidden_layer_tags):
+                    continue
+                children_layers = get_layers(layer)
+                if layer_label_in(layer, exposed_layer_tags):
+                    if children_layers:
+                        filter_layers(children_layers, append=False, show=True)
+                elif children_layers:
+                    filter_layers(children_layers, show, append)
+                elif append:
+                    frame_layers.append(layer)
+        
+        layers = get_layers(self.svg)
+        
+        if opts.firstlev:
+            filter_objects(layers)
+        else:
+            filter_layers(layers)
 
-        filter_layers(layers, self.options.firstlev)
-
-        if len(frame_layers) < 1:
+        total_count = len(frame_layers)
+        
+        if total_count == 0:
             inkex.errormsg("Nothing to do.")
-            return
-
+            return False
+            
+    # render frames
         for i,layer in enumerate(frame_layers):
-            show_parent_layers(layer)
-            show_layer(layer)
-            snapshot(self.document, dirname=self.tempdir, name=f'{i:04}',
-                     dpi=self.options.resolution, export_background_opacity=255)
-            show_layer(layer, show=False)
+            show_parent_layers(layer, show=True)
+            show_element(layer, show=True)
+            export_png(i, total_count)
+            show_element(layer, show=False)
             show_parent_layers(layer, show=False)
 
-            # copy PNG file if the option enabled
-            if self.options.pngs:
-                f = os.path.join(self.tempdir, f'{i:04}.png')
-                t = os.path.join(self.options.path, f'{i+self.options.pngn:04}.png')
-                shutil.copyfile(f, t)
+        if opts.backandforth and total_count > 2:
+            total_count = total_count * 2 - 2
 
-        # convert to mp4
-        fps   = self.options.fps
-        crf   = self.options.crf
-        codec = ['libx264', 'libx264rgb'][self.options.rgb]
-        pngs  = os.path.join(self.tempdir, '%04d.png')
-        mp4   = os.path.join(self.tempdir, 'temp.mp4')
-        loglevel  = '-hide_banner -loglevel warning'
-        vf_loops  = f'loop={self.options.loops-1}:{len(frame_layers)}'
-        args = f'''
-            ffmpeg
-                {loglevel}
-                -framerate {fps} -i {pngs}
-                -vf {vf_loops}
-                -c:v {codec} -crf {crf}
-               {mp4}
-        '''
-        sp.run(args.split())
+    # encode
+        def ffplay(what, *args):
+            if opts.ffplay:
+                try:
+                    execute(['ffplay', '-loglevel', 'quiet', *args,
+                             '-vf', f'loop=-1:{total_count}', what], None)
+                except FileNotFoundError:
+                    inkex.errormsg("`ffplay` not found,"
+                                   " the animation will not be played.")
 
-        # save output
-        with open(mp4, 'rb') as tmp4:
-            shutil.copyfileobj(tmp4, stream)
+        def extend(path, ext):
+            folder = os.path.split(path)[0]
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            if path.lower().endswith(ext):
+                ext = ''
+            return path + ext
+            
+        input = os.path.join(self.tempdir, '%04d.png')
+        ffargs = ['ffmpeg', '-y', '-hide_banner', '-loglevel', 'warning', 
+                  '-framerate', str(opts.fps), '-i', input]
+        
+        # PNG
+        if opts.codec == 'png':
+            if not os.path.exists(opts.path):
+                os.makedirs(opts.path)
+            output = os.path.join(opts.path, '%04d.png')
+            execute(ffargs + ['-c', 'copy', '-start_number', str(opts.pngn),
+                              output])
+            # `input` is played: for ffplay it's easier to play image 
+            # file sequence starting from file 0 rather than `opts.pngn`
+            ffplay(input, '-framerate', str(opts.fps))
 
-        if self.options.ffplay:
-            sp.run(f'ffplay -loglevel quiet -loop 0 {mp4}'.split())
+        # APNG
+        elif opts.codec == 'apng':
+            output = extend(opts.path, '.png')
+            execute(ffargs + ['-plays', '0', '-f', 'apng', output])
+            ffplay(output)
 
+        # GIF
+        elif opts.codec == 'gif':
+            # https://ffmpeg.org/ffmpeg-filters.html#palettegen
+            output = extend(opts.path, '.gif')
+            palette = os.path.join(self.tempdir, 'palette.png')
+            loglevel = ['-loglevel', 'error']
+            execute(ffargs + ['-vf', 'palettegen=256', palette, *loglevel])
+            execute(ffargs + ['-i', palette, '-filter_complex', 
+                              f'paletteuse=floyd_steinberg', output])
+            ffplay(output)
+
+        # other formats
+        else:
+            codec, pix_fmt, ext = {
+                'mp4'      : ('libx264',    'yuv444p',      '.mp4' ),
+                'mp4_rgb'  : ('libx264rgb', 'rgb24',        '.mp4' ),
+                'mp4_420'  : ('libx264',    'yuv420p',      '.mp4' ),
+                'webm'     : ('vp9',        'yuv444p',      '.webm'),
+                'webm_rgb' : ('vp9',        'bgrp',         '.webm'),
+                'webm_420' : ('vp9',        'yuva420p',     '.webm'),
+                'prores'   : ('prores_ks',  'yuva444p10le', '.mov' )
+            }[opts.codec]
+            
+            output = extend(opts.path, ext)
+            vf = f'format={pix_fmt},loop={opts.loops-1}:{total_count}'
+            
+            if   opts.codec.startswith('mp4'):    
+                crf = ['-crf', str(min(opts.crf, 51))]
+            elif opts.codec.startswith('webm'):   
+                crf = ['-crf', str(opts.crf), '-b:v', '0']
+            elif opts.codec.startswith('prores'): 
+                crf = ['-profile:v', '4444']  # not a crf
+            
+            execute(ffargs + ['-vf', vf, '-c:v', codec, *crf, output])
+            ffplay(output)
+
+    # document should stay unchanged
+        # Inkscape has no facility to define an `OutputExtension` which outputs
+        # in multiple formats. It is not bad in our case, because `EffectExtension`
+        # (or it's analog, in our case) can be repeatedly invoked with "Alt+Q"
+        # shotrcut without a need to redefine the parameters including a path
+        # to a video file. Another issue is that Inkscape always expects an SVG
+        # formatted string from an extension's `stdout` and will always `rebase()`
+        # (overwrite) an open document with that string, so even if an extension
+        # does not change a document it must write original one to its stdout,
+        # otherwise Inkscape complains with the 'The output from the extension
+        # could not be parsed' message.
+        # 
+        # `SvgInputMixin` stores originl SVG in `self.original_document`,
+        # `SvgOutputMixin` defines proper `self.save()` method to pass an SVG
+        # to Inkscape (over stdout) after an effect has been applied.
+        # `SvgThroughMixin` (not used in this extension), combining both the above
+        # mixins defines an unsuitable (in our case) method for testing whether
+        # a document has changed. In our case a document may not change, that's
+        # why two separate mixins are used.
+        #
+        self.document = self.original_document
+        return
+            
 if __name__ == '__main__':
-    AnimLayers().run()
+    Layers2Anim().run()
